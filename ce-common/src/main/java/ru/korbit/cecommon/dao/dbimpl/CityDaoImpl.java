d4e1f848276311a65526519d25110f1d4c260f43
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-10-12 10:23:00 +0200

    CityDao implementation

---
+++ ce-common/src/main/java/ru/korbit/cecommon/dao/dbimpl/CityDaoImpl.java
@@ -0,0 +1,26 @@
+package ru.korbit.cecommon.dao.dbimpl;
+
+import org.springframework.stereotype.Repository;
+import ru.korbit.cecommon.dao.CityDao;
+import ru.korbit.cecommon.domain.City;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Created by Artur Belogur on 11.10.17.
+ */
+@Repository(value = "cityDao")
+public class CityDaoImpl extends SessionFactoryHolder implements CityDao {
+    @Override
+    public Optional<City> getCity(Long id) {
+        return getSession().byId(City.class).loadOptional(id);
+    }
+
+    @Override
+    public Stream<City> getAllCity() {
+        return getSession()
+                .createQuery("SELECT c FROM City c", City.class)
+                .stream();
+    }
+}
