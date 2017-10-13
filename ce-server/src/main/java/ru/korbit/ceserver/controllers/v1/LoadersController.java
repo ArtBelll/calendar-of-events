a5628b8525d904de7acce34bf269a454e39dfc93
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-10-13 18:34:02 +0200

    Starter for Rambler.Kassa loader

---
+++ ce-server/src/main/java/ru/korbit/ceserver/controllers/v1/LoadersController.java
@@ -0,0 +1,42 @@
+package ru.korbit.ceserver.controllers.v1;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.korbit.cecommon.config.Constants;
+import ru.korbit.cecommon.services.RamblerKassaAsyncService;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by Artur Belogur on 13.10.17.
+ */
+@RestController
+@RequestMapping(value = "load")
+@Slf4j
+public class LoadersController {
+
+    private RedissonClient redissonClient;
+
+    @Autowired
+    public LoadersController(RedissonClient redissonClient) {
+        this.redissonClient = redissonClient;
+    }
+
+    @GetMapping(value = "rambler-kassa")
+    public ResponseEntity<?> loadRamblerKassa() throws ExecutionException, InterruptedException {
+        val remoteService = redissonClient.getRemoteService(Constants.QUEUE_NAME);
+        val ramblerKassaService = remoteService.get(RamblerKassaAsyncService.class);
+
+        log.info("Start load Ramdler.Kassa data");
+
+        ramblerKassaService.load().get();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
