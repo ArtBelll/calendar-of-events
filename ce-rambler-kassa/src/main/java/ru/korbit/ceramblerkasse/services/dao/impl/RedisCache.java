1c75eda0d321e9f350396b28d66876fd84860c89
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-10-17 13:50:26 +0200

    Init cache dao interface

---
+++ ce-rambler-kassa/src/main/java/ru/korbit/ceramblerkasse/services/dao/impl/RedisCache.java
@@ -0,0 +1,15 @@
+package ru.korbit.ceramblerkasse.services.dao.impl;
+
+import ru.korbit.ceramblerkasse.responses.RamblerCity;
+import ru.korbit.ceramblerkasse.services.dao.Cache;
+
+/**
+ * Created by Artur Belogur on 17.10.17.
+ */
+public class RedisCache implements Cache {
+
+    @Override
+    public void add(RamblerCity city) {
+
+    }
+}
