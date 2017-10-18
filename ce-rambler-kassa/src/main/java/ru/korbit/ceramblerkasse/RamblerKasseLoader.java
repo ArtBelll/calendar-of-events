5bdeec4c854f9fff610124f4592863aac3872cd5
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-10-18 10:06:05 +0200

    Refactoring

--- ce-rambler-kassa/src/main/java/ru/korbit/ceramblerkasse/RamblerKasseLoader.java
+++ ce-rambler-kassa/src/main/java/ru/korbit/ceramblerkasse/RamblerKasseLoader.java
@@ -9,6 +9,7 @@ import ru.korbit.cecommon.domain.City;
 import ru.korbit.cecommon.domain.Event;
 import ru.korbit.cecommon.services.RamblerKassaService;
 import ru.korbit.ceramblerkasse.responses.RamblerCity;
+import ru.korbit.ceramblerkasse.responses.RamblerEvent;
 import ru.korbit.ceramblerkasse.services.api.impl.RamblerKassa;
 import ru.korbit.ceramblerkasse.services.filters.RedisRegion;
 import ru.korbit.ceramblerkasse.services.filters.impl.CheckerExistCacheImpl;
@@ -50,46 +51,64 @@ public class RamblerKasseLoader implements RamblerKassaService {
         cities.stream()
                 .filter(city -> city.getCityRamblerId().equals(KALININGRAD_ID))
                 .forEach(ramblerCity -> {
-                    final City currentCity;
 
-                    val cityDbId = checkerExistInCache.check(ramblerCity.getCityRamblerId().longValue(),
-                            RedisRegion.CITY);
-                    if (cityDbId.isPresent()) {
-                        currentCity = checkerExistInDb.getObject(cityDbId.get(), City.class);
-                    } else {
-                        currentCity = checkerExistInDb.checkAndSave(ramblerCity.toDBCity());
-                        checkerExistInCache.save(currentCity.getId(),
-                                ramblerCity.getCityRamblerId().longValue(), RedisRegion.CITY);
-                    }
+                    val currentCity = getCurrentCity(ramblerCity);
 
-                    ramblerKassa.getCinemasAtCity(ramblerCity.getCityRamblerId())
-                            .forEach(ramblerCinema -> {
-                                val cinemaDbId = checkerExistInCache
-                                        .check(ramblerCinema.getCinemaRamblerId().longValue(), RedisRegion.CINEMA);
-                                if (!cinemaDbId.isPresent()) {
-                                    val cinemaDb = ramblerCinema.toBDCinema();
-                                    cinemaDb.setCity(currentCity);
-                                    checkerExistInDb.checkAndSave(cinemaDb);
-                                }
-                            });
+                    checkExistCinemasAtCity(ramblerCity.getCityRamblerId(), currentCity);
 
-                    ramblerKassa.getEventsLessDateAtCity(ramblerCity.getCityRamblerId(), MAX_DATE)
-                            .forEach(ramblerEvent -> {
-                                final Event currentEvent;
-
-                                val eventDbId = checkerExistInCache
-                                        .check(ramblerEvent.getEventRamblerId().longValue(), RedisRegion.EVENT);
-                                if (eventDbId.isPresent()) {
-                                    currentEvent = checkerExistInDb.getObject(eventDbId.get(), Event.class);
-                                }
-                                else {
-                                    currentEvent = checkerExistInDb.checkAndSave(ramblerEvent.toDBEvent());
-                                    checkerExistInCache.save(currentEvent.getId(),
-                                            ramblerEvent.getEventRamblerId().longValue(), RedisRegion.EVENT);
-                                }
-                            });
-
-
+                    loadEvents(ramblerCity, currentCity);
                 });
+    }
+
+    private void checkExistCinemasAtCity(Integer cityRamblerId, City currentCity) {
+        ramblerKassa.getCinemasAtCity(cityRamblerId)
+                .forEach(ramblerCinema -> {
+                    val cinemaDbId = checkerExistInCache
+                            .check(ramblerCinema.getCinemaRamblerId().longValue(), RedisRegion.CINEMA);
+                    if (!cinemaDbId.isPresent()) {
+                        val cinemaDb = ramblerCinema.toBDCinema();
+                        cinemaDb.setCity(currentCity);
+                        checkerExistInDb.checkAndSave(cinemaDb);
+                    }
+                });
+    }
+
+    private void loadEvents(RamblerCity ramblerCity, City currentCity) {
+        ramblerKassa.getEventsLessDateAtCity(ramblerCity.getCityRamblerId(), MAX_DATE)
+                .forEach(ramblerEvent -> {
+                    val currentEvent = getCurrentEvent(ramblerEvent);
+                });
+    }
+
+    private City getCurrentCity(RamblerCity ramblerCity) {
+        final City currentCity;
+
+        val cityDbId = checkerExistInCache.check(ramblerCity.getCityRamblerId().longValue(),
+                RedisRegion.CITY);
+        if (cityDbId.isPresent()) {
+            currentCity = checkerExistInDb.getObject(cityDbId.get(), City.class);
+        } else {
+            currentCity = checkerExistInDb.checkAndSave(ramblerCity.toDBCity());
+            checkerExistInCache.save(currentCity.getId(),
+                    ramblerCity.getCityRamblerId().longValue(), RedisRegion.CITY);
+        }
+
+        return currentCity;
+    }
+
+    private Event getCurrentEvent(RamblerEvent ramblerEvent) {
+        final Event currentEvent;
+
+        val eventDbId = checkerExistInCache
+                .check(ramblerEvent.getEventRamblerId().longValue(), RedisRegion.EVENT);
+        if (eventDbId.isPresent()) {
+            currentEvent = checkerExistInDb.getObject(eventDbId.get(), Event.class);
+        } else {
+            currentEvent = checkerExistInDb.checkAndSave(ramblerEvent.toDBEvent());
+            checkerExistInCache.save(currentEvent.getId(),
+                    ramblerEvent.getEventRamblerId().longValue(), RedisRegion.EVENT);
+        }
+
+        return currentEvent;
     }
 }
