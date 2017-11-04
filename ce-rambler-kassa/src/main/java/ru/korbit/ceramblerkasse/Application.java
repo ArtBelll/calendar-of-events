02368a2c19d915682e1e7dcfb5786f0004ef3b92
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-11-05 00:02:23 +0200

    Fix config

--- ce-rambler-kassa/src/main/java/ru/korbit/ceramblerkasse/Application.java
+++ ce-rambler-kassa/src/main/java/ru/korbit/ceramblerkasse/Application.java
@@ -1,6 +1,5 @@
-package ru.korbit.ceserver;
+package ru.korbit.ceramblerkasse;
 
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -12,32 +11,37 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import ru.korbit.cecommon.config.Constants;
+import ru.korbit.cecommon.services.RamblerKassaService;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import java.io.IOException;
 
 /**
- * Created by Artur Belogur on 02.11.17.
+ * Created by Artur Belogur on 13.10.17.
  */
-@SpringBootApplication(scanBasePackages = {"ru.korbit.ceserver", "ru.korbit.cecommon"},
+@SpringBootApplication(scanBasePackages = {"ru.korbit.ceramblerkasse", "ru.korbit.cecommon"},
         exclude = {HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
 @Slf4j
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+    public static void main(final String[] args) {
+        if (args.length > 0 && args[0].equals("dev")) {
+            Environment.host = Environment.PROXY;
+        }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        val app = SpringApplication.run(Application.class, args);
+        val redissonClient = app.getBean(RedissonClient.class);
+        val ramblerKasseLoader = app.getBean(RamblerKasseLoader.class);
+        val service = redissonClient.getRemoteService(Constants.QUEUE_NAME);
+        service.register(RamblerKassaService.class, ramblerKasseLoader, 2);
     }
 
     @Value(value = "classpath:redisson.json")
@@ -50,13 +54,6 @@ public class Application extends SpringBootServletInitializer {
     public RedissonClient redissonClient() throws IOException {
         val config = Config.fromJSON(redissonConfig.getInputStream());
         return Redisson.create(config);
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilder jacksonBuilder() {
-        Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
-        b.modulesToInstall(new GuavaModule());
-        return b;
     }
 
     @Bean
@@ -73,5 +70,4 @@ public class Application extends SpringBootServletInitializer {
         txManager.setSessionFactory(sessionFactory);
         return txManager;
     }
-
 }
