980f76455a9df520a4e1433b96f0259c202d8001
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-11-07 11:02:19 +0200

    Rename keys for redis store

--- ce-rambler-kassa/src/main/java/ru/korbit/ceramblerkasse/services/store/impl/RedisRegion.java
+++ ce-rambler-kassa/src/main/java/ru/korbit/ceramblerkasse/services/store/impl/RedisRegion.java
@@ -6,12 +6,12 @@ import ru.korbit.ceramblerkasse.services.store.Region;
  * Created by Artur Belogur on 17.10.17.
  */
 public enum RedisRegion implements Region {
-    CITY("city"),
-    CINEMA("cinema"),
-    HALL("hall"),
-    SHOWTIME("showtime"),
-    EVENT("event"),
-    EVENT_TYPE("event_type");
+    CITY("rambler-city"),
+    CINEMA("rambler-cinema"),
+    HALL("rambler-hall"),
+    SHOWTIME("rambler-showtime"),
+    EVENT("rambler-event"),
+    EVENT_TYPE("rambler-event-type");
 
     private final String region;
 
