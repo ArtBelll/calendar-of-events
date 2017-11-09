c114d50c287ecf2dd8fc619b3cfbcbec6448f0a0
Artur Belogur <artur.belogur.korbit@gmail.com>
2017-11-09 13:02:47 +0200

    Init module that is responsible for downloading from third-party resources

---
+++ ce-loader/src/main/java/ru/korbit/celoader/Application.java
@@ -0,0 +1,60 @@
+package ru.korbit.celoader;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.hibernate.SessionFactory;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import java.io.IOException;
+
+/**
+ * Created by Artur Belogur on 13.10.17.
+ */
+@SpringBootApplication(scanBasePackages = {"ru.korbit.celoader", "ru.korbit.cecommon"},
+        exclude = {HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
+@Slf4j
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value(value = "classpath:redisson.json")
+    private Resource redissonConfig;
+
+    @Value(value = "classpath:hibernate.cfg.xml")
+    private Resource hibernateProperties;
+
+    @Bean
+    public RedissonClient redissonClient() throws IOException {
+        val config = Config.fromJSON(redissonConfig.getInputStream());
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(HikariDataSource dataSource) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setConfigLocation(hibernateProperties);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+        return txManager;
+    }
+}
