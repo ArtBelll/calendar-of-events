5e89959106903d679b1bb836a0b69699718d2ba8
Artur Belogur <artur.belogur@korbit.ru>
2017-11-15 14:15:41 +0200

    Init ce-admin module

---
+++ ce-admin/src/main/java/ru/korbit/ceadmin/Application.java
@@ -0,0 +1,70 @@
+package ru.korbit.ceadmin;
+
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.hibernate.SessionFactory;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import java.io.IOException;
+
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class})
+@Slf4j
+@ComponentScan(value = {"ru.korbit.ceadmin", "ru.korbit.cecommon"})
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value(value = "classpath:redisson.json")
+    private Resource redissonConfig;
+
+    @Value(value = "classpath:hibernate.cfg.xml")
+    private Resource hibernateProperties;
+
+    @Bean
+    public RedissonClient redissonClient() throws IOException {
+        val config = Config.fromJSON(redissonConfig.getInputStream());
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(HikariDataSource dataSource) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setConfigLocation(hibernateProperties);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+        return txManager;
+    }
+
+}
