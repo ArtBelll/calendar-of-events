9e9c4bacb418f4a0ed4e1678dc11a38ecc164b29
Artur Belogur <artur.belogur@korbit.ru>
2017-11-29 11:09:20 +0200

    Constants for images

--- ce-common/src/main/java/ru/korbit/cecommon/config/Constants.java
+++ ce-common/src/main/java/ru/korbit/cecommon/config/Constants.java
@@ -1,5 +1,8 @@
 package ru.korbit.cecommon.config;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Artur Belogur on 13.10.17.
  */
@@ -8,4 +11,16 @@ public abstract class Constants {
     private Constants() { }
 
     public static final String QUEUE_NAME = "ce-queue";
+
+    public static final String IMAGE_DIR = "/ce-images/";
+    public static final String IMAGE_BASE_URL = "http://localhost:8082/%s";
+
+    public static URL getDefaultImage() {
+        try {
+            return new URL("https://www.technodoze.com/wp-content/uploads/2016/03/default-placeholder.png");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
